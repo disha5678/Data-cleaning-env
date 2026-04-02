1adb9e7475811fba6c5c0c554a9c808006d817e6
disha <dishasingla2005@gmail.com>
2026-04-02 18:59:44 +0530

    added functions and tested them

--- env/graders/task1_grader.py
+++ env/graders/task1_grader.py
@@ -1,22 +1,22 @@
-def grade_task1(agent_df, clean_df):
+def grade_task1(agent_df, clean_df, manifest):
     score = 0
 
-    # duplicates
-    if len(agent_df) == len(clean_df):
-        score += 0.25
-
-    # nulls
+    # null handling
     if agent_df.isnull().sum().sum() == 0:
         score += 0.25
 
-    # dtype
+    # duplicates
+    if len(agent_df.drop_duplicates()) == len(clean_df):
+        score += 0.25
+
+    # dtype check
     try:
         agent_df["age"].astype(int)
         score += 0.25
     except:
         pass
 
-    # whitespace check (basic)
+    # whitespace (basic assumption)
     score += 0.25
 
-    return score
+    return min(score, 1.0)
